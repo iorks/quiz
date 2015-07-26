4fddb4121eae14fbe9bcc6c6d268cfc02bd88bff
Jorge Benitez <iorkss@gmail.com>
2015-07-27 00:34:39 +0200

    Borrado de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -66,6 +66,13 @@ exports.update = function(req, res){
 	});
 };
 
+// DELETE /quizes/:id
+exports.destroy = function(req, res){
+	req.quiz.destroy().then(
+		function(){
+			res.redirect('/quizes');
+	}).catch(function(error) { next(error); });
+};
 
 // GET /quizes
 exports.index = function(req, res){
