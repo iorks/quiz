229f01960626a90a48b81a41e3e2c6a651467ff5
Jorge Benitez <iorkss@gmail.com>
2015-08-12 21:03:57 +0200

    Autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
-var commentController = require('../controllers/comment_controller')
+var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 //Pagina de entrada (home page)
 router.get('/', function(req, res) {
@@ -12,6 +13,12 @@ router.get('/', function(req, res) {
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
 
+//Definicion de rutas de sesión
+router.get('/login', sessionController.new);		//formulario de login
+router.post('/login', sessionController.create);	//crear sesion
+router.get('/logout', sessionController.destroy);	//destruir sesion
+
+//Definicion de rutas de quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
@@ -23,6 +30,7 @@ router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
 router.get('/author', quizController.author);
 
+//Definicion de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 
